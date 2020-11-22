7d7ae73b94d72c4e3b7c7765e3574ea105d980d1
thomas <thomas.estiez@gmail.com>
2020-11-22 18:36:18 +0100

    fix: fixed binary name in benchmark

--- benchmark.ps1
+++ benchmark.ps1
@@ -1,4 +1,13 @@
 $image_file = $args[0]
-bazel-bin/example_classification/classification.exe --image_file=$image_file --number_of_inferences=1000
-bazel-bin/example_classification/classification.exe --inference_mode=GPU --image_file=$image_file --number_of_inferences=1000
-python37 .\example_classification\example_classification.py --image_file=$image_file --number_of_inferences=1000
+echo "==================================================="
+echo "TFLite C++ CPU inference =========================="
+echo "==================================================="
+bazel-bin/example_classification/classification_cpu.exe --image_file=$image_file --number_of_inferences=100
+echo "==================================================="
+echo "TFLite C++ GPU inference =========================="
+echo "==================================================="
+bazel-bin/example_classification/classification_gpu.exe --image_file=$image_file --number_of_inferences=100
+echo "==================================================="
+echo "TFLite Python CPU inference ======================="
+echo "==================================================="
+python37 .\example_classification\example_classification.py --image_file=$image_file --number_of_inferences=100
